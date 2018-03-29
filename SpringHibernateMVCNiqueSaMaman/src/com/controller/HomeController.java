a78740a4260d7ada8ae230ae21f390033001feef
Boris <Boris.denis76@free.fr>
2018-03-29 13:26:32 +0200

    aas

--- SpringHibernateMVCNiqueSaMaman/src/com/controller/HomeController.java
+++ SpringHibernateMVCNiqueSaMaman/src/com/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HomeController2 {
+public class HomeController {
 
 	@RequestMapping("/")
 	public String homePage() {
